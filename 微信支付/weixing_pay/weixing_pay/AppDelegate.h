62da684be147a83ca56d285bc248fc195638ee26
likangbin <1218773641@qq.com>
2016-10-11 16:18:21 +0800

    微信支付_第一次提交

---
+++ 微信支付/weixing_pay/weixing_pay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weixing_pay
+//
+//  Created by Apple on 16/10/11.
+//  Copyright © 2016年 lkb-求工作qq:1218773641. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微信支付/weixing_pay/weixing_pay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  weixing_pay
+//
+//  Created by Apple on 16/10/11.
+//  Copyright © 2016年 lkb-求工作qq:1218773641. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
